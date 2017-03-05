514bb42feb7f097629d9b2852e2f7566eb37cc21
Anh Nguyen <Anh Nguyen>
2017-03-05 00:32:44 -0500

    remove example folder

--- MEAN STACK/MEAN/Examples/helloCode/talk/question.js
+++
@@ -1,6 +0,0 @@
-var answer = "This is the answer";
-
-module.exports.ask = function(question){
-	console.log(question);
-	return answer;
-};
--- MEAN STACK/MEAN/Examples/nonBlockingCode/setTimeout.js
+++
@@ -1,9 +0,0 @@
-console.log('1: Start app\n');
-
-//This function will run after timeout 1000 ms
-var holdOn = setTimeout(function(){
-	console.log('2: In the setTimeout\n');
-}, 1000);
-
-
-console.log('3: End app\n');
